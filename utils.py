b46fd93ef3c64fcff97c4711f47b41758774f098
Touko <oranly@hotmail.com>
2020-09-22 17:29:36 +0800

    Adapt for windows.

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ import sys
 import time
 import math
 
+import torch
 import torch.nn as nn
 import torch.nn.init as init
 
@@ -42,7 +43,8 @@ def init_params(net):
                 init.constant(m.bias, 0)
 
 
-_, term_width = os.popen('stty size', 'r').read().split()
+# _, term_width = os.popen('stty size', 'r').read().split()
+term_width = 80
 term_width = int(term_width)
 
 TOTAL_BAR_LENGTH = 65.
